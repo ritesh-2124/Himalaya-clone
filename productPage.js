4f64d4a7d30246727016d772e6814a177e45532b
iamrituyadav <iamrituyadav@outlook.com>
2021-12-16 22:39:26 +0530

    Added data to localStorage for Cart

--- productPage.js
+++ productPage.js
@@ -1,10 +1,9 @@
 var productData = JSON.parse(localStorage.getItem("product")) || [];
+var cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 productData.map(productCard);
 
 function productCard(item) {
-    console.log(item);
-
   var div = document.createElement("div");
   var img = document.createElement("img");
   img.setAttribute("src", item.disc);
@@ -18,6 +17,9 @@ function productCard(item) {
   var i = document.createElement("i");
   i.setAttribute("class", "fas fa-shopping-bag");
   btn.append(i);
+  btn.addEventListener("click", function(){
+      addToCart(item);
+  });
 
   var price = document.createElement("h3");
   price.textContent = "$" + item.priceof;
@@ -27,3 +29,12 @@ function productCard(item) {
   div.append(img, title, childDiv);
   document.querySelector("#product").append(div);  
 }
+
+function addToCart(item) {
+    cart.push(item);
+    var cartItem = localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+
+
+
